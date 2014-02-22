8dc94325569c0887d641580aa4ae136cb92f771e
Michael Rapadas <mike@mrap.me>
2014-02-21 16:15:06 -0800

    Tag has unique name

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag
   include Mongoid::Document
 
   has_many :activities
+  field :name
+
+  validates_uniqueness_of :name
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe Tag do
   it { should have_many :activities }
+  it { should have_field :name }
+  it { should validate_uniqueness_of :name }
 end
