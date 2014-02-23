ea80ef11521b3cccfb458bd0121fca1e308bd4c6
Michael Rapadas <mike@mrap.me>
2014-02-23 06:07:05 -0800

    Basic working Push Notifications.

--- app/models/push_notifier.rb
+++ app/models/push_notifier.rb
@@ -5,11 +5,19 @@ class PushNotifier
     APNS::Notification.new(device_token, options)
   end
 
-  def self.notify_users(users = [])
+  def self.notify_users(users, options = {})
+    users ||= []
     notifications = []
     users.each do |u|
-      notifications << PushNotifier.new_notification(u.device_token) if u.device_token
+      if u.device_token
+        notification = PushNotifier.new_notification(u.device_token, options)
+        notifications << notification
+        puts "Preparing Notification: " + notification.inspect
+      else
+        puts "No Notification: no device token"
+      end
     end
+    puts "Sending #{notifications.count} Notifications"
     APNS.send_notifications(notifications)
   end
 
--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -1,4 +1,4 @@
-APNS.host = 'gateway.push.apple.com'
+# APNS.host = 'gateway.push.apple.com' # production only
 APNS.port = 2195
 APNS.pem = Rails.root.join('cert.pem')
 APNS.pass = ENV['APNS_PASS']
